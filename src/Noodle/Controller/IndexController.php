4eea089b578fbcc346e667830fdffec79065a41e
Jan Tlapák <jan@potentus.com>
2013-07-30 20:34:08 +0200

    pre release

--- src/Noodle/Controller/IndexController.php
+++ src/Noodle/Controller/IndexController.php
@@ -16,17 +16,6 @@ class IndexController extends AbstractController
 {
     public function indexAction()
     {
-    	//$modules = $this->getServiceLocator()->get('modulesService')->getModules();
-		/*
-    	$authService = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
-
-    	$adapter = $authService->getAdapter();
-    	$adapter->setIdentityValue('jan@potentus.com');
-    	$adapter->setCredentialValue(md5('hovno'));
-    	$authResult = $authService->authenticate();
-
-    	var_dump($authResult);*/
-
     	$this->getEventManager()->trigger('dashboard.load', $this);
 
         return new ViewModel(array(
