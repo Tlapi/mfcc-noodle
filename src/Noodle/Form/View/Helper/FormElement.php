0216348a2e474bb9b4f5bcb2bf78a92ba9377c02
Jan Šourek <jan.sourek@gmail.com>
2014-04-04 00:03:01 +0200

    rendering of datetime

--- src/Noodle/Form/View/Helper/FormElement.php
+++ src/Noodle/Form/View/Helper/FormElement.php
@@ -11,7 +11,7 @@ class FormElement extends BaseFormElement
 	{
 		$renderer = $this->getView();
 
-		if (!method_exists($renderer, 'plugin')) {
+        if (!method_exists($renderer, 'plugin')) {
 			// Bail early if renderer is not pluggable
 			return '';
 		}
@@ -20,6 +20,23 @@ class FormElement extends BaseFormElement
 			$helper = $renderer->plugin('formPicture');
 			return $helper($element);
 		}
+
+        //die(var_dump($element));
+        if ($element instanceof \Zend\Form\Element\Text ||
+            $element instanceof \Zend\Form\Element\Radio ||
+            $element instanceof \Zend\Form\Element\Checkbox) {
+        }
+        else {
+
+            if(get_class($element->getValue()) == "DateTime")
+            {   //var_dump($element);
+                $helper = $renderer->plugin('formDateTime');
+                //var_dump($helper);
+                //die('helper');
+                //return "date time";
+                return $helper($element);
+            }
+        }
 
 		return parent::render($element);
 	}
